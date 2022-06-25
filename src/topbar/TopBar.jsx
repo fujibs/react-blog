10499bd42a8d87c40aaf86100b6a8f8898eae989
Brandon Saltzman <59933684+fujibs@users.noreply.github.com>
2022-06-24 23:18:53 -0400

    social media

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,26 @@
+import "./topbar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+        <div className="topLeft">
+            <i className="socialMediaIcon" class="fa-brands fa-facebook-square"></i>
+            <i className="socialMediaIcon" class="fa-brands fa-instagram-square"></i>
+            <i className="socialMediaIcon" class="fa-brands fa-pinterest-square"></i>
+            <i className="socialMediaIcon" class="fa-brands fa-twitter-square"></i>
+        </div>
+        <div className="topCenter">
+            <ul className="topList">
+                <li className="topListItem">HOME</li>
+                <li className="topListItem">ABOUT</li>
+                <li className="topListItem">CONTACT</li>
+                <li className="topListItem">WRITE</li>
+                <li className="topListItem">LOGOUT</li>
+            </ul>
+        </div>
+        <div className="topRight">
+
+        </div>
+    </div>
+  )
+}
