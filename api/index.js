e06c41ee786ea19f4a06ea1acf289c5f82125166
rob <robsanchez1772@gmail.com>
2022-07-02 11:55:38 -0700

    rob fixes

--- api/index.js
+++ api/index.js
@@ -7,33 +7,46 @@ const userRoute = require("./routes/users.js");
 const postRoute = require("./routes/posts.js");
 const categoryRoute = require("./routes/categories.js");
 const multer = require("multer");
+const cors = require("cors");
 
 dotenv.config();
 app.use(express.json());
 
+/**
+ * enable cross domain requests
+ */
+app.use(cors());
+
 mongoose
-    .connect(process.env.MONGO_URL,)
-    .then(console.log("Connected to url"))
-    .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to url"))
+  .catch((err) => console.log(err));
 
-    const storage = multer.diskStorage({
-        destination:(req, file, callback) => {
-            callback(null, "images");
-        }, filename:(req, file, callback) => {
-            callback(null, res.body.name);
-        },
-    });
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, res.body.name);
+  },
+});
 
-    const upload = multer({storage:storage});
-    app.post("/api/upload", upload.single("file"), (req,res) => {
-        res.status(200).json("File has been uploaded");
-    });
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
-    app.use("/api/auth", authRoute);
-    app.use("/api/users", userRoute);
-    app.use("/api/posts", postRoute);   
-    app.use("/api/categories", categoryRoute);   
-    
-app.listen("3000", ()=>{
-    console.log("Connected");
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+/**
+ * Change port to 8000 since our react app
+ * is already listening on port 3000
+ */
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log("Connected on PORT ", PORT);
 });
--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -7,23 +7,66 @@ import axios from "axios";
 import { useLocation } from "react-router-dom";
 
 export default function Homepage() {
+  // holds all blog posts
   const [posts, setPosts] = useState([]);
 
+  /**
+   * Fetch all blogs on component is loaded
+   */
   useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await axios.get("/posts/");
-      setPosts(res.data);
+    let mounted = true;
+
+    /**
+     * Makes API request for journal data
+     * and sets data in state
+     *
+     * we type 'async' in front of 'function'
+     * so that we can use 'await' within the scope of the function
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+     */
+    async function fetchDataAsync() {
+      console.log("go");
+      /**
+       * We use try/catch to handle our promise results
+       * or errors.
+       * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+       */
+      try {
+        const url = "http://localhost:8080/api/posts";
+        const res = await axios.get(url); // fetch data
+        setPosts(res.data);
+        console.log(res.data);
+      } catch (e) {
+        // handle your errors here
+        console.log(e);
+      }
     }
-    fetchPosts();
-  },[]);
+
+    if (mounted) fetchDataAsync();
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  /**
+   * Old code
+   */
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const res = await axios.get("/posts/");
+  //     setPosts(res.data);
+  //   };
+  //   fetchPosts();
+  // }, []);
 
   return (
     <>
       <Header />
       <div className="homepage">
-        <Posts posts={posts}/>
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
-  )
+  );
 }
