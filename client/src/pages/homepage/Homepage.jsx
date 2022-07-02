95dad5d2105e91ecf1e81f0744a0b4897f39c5d3
Brandon Saltzman <59933684+fujibs@users.noreply.github.com>
2022-07-02 14:04:09 -0400

    latest

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -4,17 +4,19 @@ import Posts from "../../components/posts/Posts.jsx";
 import Sidebar from "../../components/sidebar/Sidebar.jsx";
 import "./homepage.css";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 export default function Homepage() {
   const [posts, setPosts] = useState([]);
 
-  useEffect(()=> {
-      const fetchPosts = async () =>{
-        const res = await axios.get("/api/posts");
-        setPosts(res.data)
-      }
-      fetchPosts()
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts/");
+      setPosts(res.data);
+    }
+    fetchPosts();
   },[]);
+
   return (
     <>
       <Header />
