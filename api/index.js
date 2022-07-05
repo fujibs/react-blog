67c020f18c19f99a381dfb2b09f8a29a498a6065
Brandon Saltzman <59933684+fujibs@users.noreply.github.com>
2022-07-05 18:47:30 -0400

    Revert "Revert "my first commit""
    This reverts commit 9ef2faa9798c91d3dc5d0ae1dd46bc323d206584.

--- api/index.js
+++ api/index.js
@@ -39,8 +39,12 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-const PORT = 8080;
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.listen(PORT, () => {
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => {
   console.log("Connected on PORT", PORT);
 });
